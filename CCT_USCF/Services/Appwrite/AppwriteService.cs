51cd37e844438d5972adc227fe073ec010004fd5
Yusuph Rowland <programmingwithyusuphmwaseba971@gmail.com>
2026-08-30 18:00:05 +0300

    working-with-real-chatting-systen

--- CCT_USCF/Services/Appwrite/AppwriteService.cs
+++ CCT_USCF/Services/Appwrite/AppwriteService.cs
@@ -1,16 +1,20 @@
 using Appwrite;
 using Appwrite.Services;
+
 namespace CCT_USCF.Services.Appwrite;
+
 public sealed class AppwriteService
 {
     public Client Client { get; }
     public Databases Databases { get; }
     public Storage Storage { get; }
+
     public AppwriteService()
     {
         Client = new Client()
             .SetEndpoint(AppwriteConfig.Endpoint)
             .SetProject(AppwriteConfig.ProjectId);
+
         Databases = new Databases(Client);
         Storage = new Storage(Client);
     }
